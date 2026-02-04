868ddd04ddce285d70351b3997b4f9e55d23f063
gokulanands05 <gokulanands03@gmail.com>
2026-02-04 11:27:55 +0530

    Day 02 : java and cyber data handling basicss

---
+++ java-basics/day02/VariableDemo.java
@@ -0,0 +1,12 @@
+class VariableDemo {
+    public static void main(String[]args){
+
+        // variable decalration
+
+        int age =20;
+        String name= "Gokulanand";
+
+    System.out.println("My name is:" + name);
+    System.out.println("My age is:" + age);
+    }
+}
