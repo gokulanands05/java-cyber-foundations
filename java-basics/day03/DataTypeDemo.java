8a6cddfc125c42f0887201e68b801c290323026b
gokulanands05 <gokulanands03@gmail.com>
2026-02-05 18:10:10 +0530

    Day 03: Java data types and cyber data type awareness

---
+++ java-basics/day03/DataTypeDemo.java
@@ -0,0 +1,18 @@
+class DataTypeDemo{
+    public static void main(String[]args){
+
+        int regno = 100;
+        String name = "gokulanand";
+        float  mark = 84.7f;
+        char grade = 'A';
+        boolean studentpresent = true;
+
+        System.out.println(regno);
+        System.out.println(name);
+        System.out.println(mark);
+        System.out.println(grade);
+        System.out.println(studentpresent);
+
+            
+    }
+}
