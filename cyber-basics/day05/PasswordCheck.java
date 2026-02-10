6e3ee6ba0914a11d7b0701ca6ffe9b096260347b
gokulanands05 <gokulanands03@gmail.com>
2026-02-10 20:28:04 +0530

    Day 05: Comparison operators and password validation logic

---
+++ cyber-basics/day05/PasswordCheck.java
@@ -0,0 +1,16 @@
+
+// password checking 
+
+class PasswordCheck{
+    public static void main (String[]args){
+
+    String password = "Welcome123";
+
+    if(password.length() >=8){
+        System.out.println("password is strong");
+    }
+        else {
+            System.out.println("password is not strong try to type strong");
+        }
+    }
+}
---
+++ java-basics/day05/ComparisonDemo.java
@@ -0,0 +1,16 @@
+// Day 05 - Comparison Operators
+
+class ComparisonDemo {
+    public static void main(String[] args) {
+
+        int a = 10;
+        int b = 20;
+
+        System.out.println(a == b);
+        System.out.println(a != b);
+        System.out.println(a > b);
+        System.out.println(a < b);
+        System.out.println(a >= b);
+        System.out.println(a <= b);
+    }
+}
