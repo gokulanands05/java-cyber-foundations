be0af1f58cc770504502547329eb1eccd2546841
gokulanands05 <gokulanands03@gmail.com>
2026-02-27 20:23:07 +0530

    day05

---
+++ JAVA_6months_roadmap/day5/datatypes.java
@@ -0,0 +1,21 @@
+class datatypes {
+    public static void main(String[] args) {
+        // Primitive data types
+        int myInt = 10; // Integer
+        double myDouble = 3.14; // Floating-point number
+        char myChar = 'A'; // Character
+        boolean myBoolean = true; // Boolean
+
+        // Non-primitive data types (reference types)
+        String myString = "Hello, World!"; // String
+ 
+        // Output the values
+        System.out.println("Integer: " + myInt);
+        System.out.println("Double: " + myDouble);
+        System.out.println("Character: " + myChar);
+        System.out.println("Boolean: " + myBoolean);
+        System.out.println("String: " + myString);
+        
+         
+    }
+}
